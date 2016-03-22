f9e8edb9c9dd1d358fec3765681642ecc686c0ca
joegotflow83 <joe@absolutod.com>
2016-03-22 13:55:38 -0400

    refactor

--- guitar_tabs/main/urls.py
+++ guitar_tabs/main/urls.py
@@ -4,6 +4,6 @@ from main import views
 
 
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^$', views.Search.as_view(), name='index'),
     url(r'^(?P<url>.*)/$', views.Tabs.as_view(), name='tabs'),
 ]
--- guitar_tabs/main/views.py
+++ guitar_tabs/main/views.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup
 import requests
 
 
-class Index(TemplateView):
+class Search(TemplateView):
     """Page to display search for guitar tabs"""
     template_name = 'main/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        search = self.request.GET['search']
+        search = self.request.GET.get('search', False)
         contents = requests.get("http://www.guitartabs.cc/search.php?song={}".format(search)).content
         soup = BeautifulSoup(contents, "html.parser").find(class_='tabslist')
         context['songs'] = soup.prettify()
